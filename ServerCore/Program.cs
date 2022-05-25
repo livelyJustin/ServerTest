9ac2e8b99ba68c4d6a749e9e2c5ab6f13459a9d3
JuseokLee <livelyjuseok@gmail.com>
2022-05-26 06:38:00 +0900

    ThreadPool, Task

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -1,34 +1,32 @@
-﻿using System;
-using System.Threading;
+﻿using System.Threading;
+using System.Threading.Tasks;
 
 namespace ServerCore
 {
     class Program
     {
-        // 기존에 메인이라는 직원(쓰레드)이 있었고
         static void Main(string[] args)
         {
-            // thread라는 새로운 직원을 뽑아 어떤 것을 하라는 Start 명령을 내린 것이다.
-            Thread thread = new Thread(MainThread);
-            thread.IsBackground = true;
-            thread.Name = "FirstThread";
-            thread.Start();
-            Console.WriteLine("기다리는 중메인 하이용");
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMaxThreads(3, 3);
 
-            thread.Join();
-            Console.WriteLine("그냥 메인 하이용");
-        }
+            for (int i = 0; i < 3; i++)
+            {
+                Task t = new Task(() => { while (true) { } },TaskCreationOptions.LongRunning);
+                t.Start();
+            }
 
-        static void MainThread()
-        {
-            int t = 0;
-            while(t < 3)
+            ThreadPool.QueueUserWorkItem(MainThread);
+
+            while(true)
             {
 
-                Console.WriteLine("메인 쓰레드 하이용");
-            t++;
             }
         }
-    }
 
+        static void MainThread(object state)
+        {
+            Console.WriteLine("메인 쓰레드 하이용");
+        }
+    }
 }
