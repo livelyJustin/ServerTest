cb760ccf815d8f767f2f7d3a84dfcbaf174420b0
liveljuseok <livelyjuseok@gmail.com>
2022-06-11 01:12:57 +0900

    Socket Programming

--- DummyClient/Program.cs
+++ DummyClient/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Dummy Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace DummyClient
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // DNS 
+            string host = Dns.GetHostName(); // 내 로컬 컴퓨터의 호스트 이름
+            IPHostEntry iPHost = Dns.GetHostEntry(host); // 
+            IPAddress ipAddr = iPHost.AddressList[0]; // 원하던 아이피 주소, 아이피가 여러개이기에 배열
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 2222); // 식당 주소, 뒷문 후문인지는 똑같음
+
+            // 연락할 휴대폰 생성
+            Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                // 입장 요청 endpoint 로 입장 문의를하는것
+                socket.Connect(endPoint);  // 주의 실제로 이러면 계속 대기함
+                Console.WriteLine($"Connected To {socket.RemoteEndPoint.ToString()} ");
+                // 어떤 녀석한테 연결 됐나 확인
+
+                // 보낸다음 받을 예정
+                byte[] sendBuff = Encoding.UTF8.GetBytes("서버야.. 자니..?"); // string을 bytes 타입으로 변경
+                int sendBytes = socket.Send(sendBuff); // 주의 실제로 이러면 계속 대기함
+
+                // 받는다 서버가 나한테 얼마나 보낼지 모르늬 크게 만든다
+                byte[] recvBuff = new byte[1024]; 
+                int recvBytes = socket.Receive(recvBuff); // 주의 실제로 이러면 계속 대기함
+                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                Console.WriteLine($"[From Server ] {recvData}");
+
+                //
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Somthing Wrong...");
+            }
+        }
+    }
+}
--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -1,35 +1,66 @@
-﻿namespace ServerCore
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerCore
 {
     public class Program
     {
-        static ThreadLocal<String> _threadLocal = new ThreadLocal<String>(
-            () => { return $"My Name is:  {Thread.CurrentThread.ManagedThreadId}"; }  );
-
-        static void WhoAmI()
-        {
-            bool repeat = _threadLocal.IsValueCreated;
-
-            if (repeat)
-                Console.WriteLine("중복임다" + _threadLocal.Value);
-            else
-                Console.WriteLine(_threadLocal.Value);
-        }
-
-
-
         static void Main(string[] args)
         {
-            Parallel.Invoke(WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI);
+            // DNS (Domain Name System) 
+            // 하드 코딩으로 IP를 등록해두면 나중에 교체 등의 문제가 생길 수 있어 도메인으로 등록
 
-            _threadLocal.Dispose();
+            // DNS 
+            string host = Dns.GetHostName(); // 내 로컬 컴퓨터의 호스트 이름
+            IPHostEntry iPHost = Dns.GetHostEntry(host); // 
+            IPAddress ipAddr = iPHost.AddressList[0]; // 원하던 아이피 주소, 아이피가 여러개이기에 배열
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 2222); // 2222는 포트 번호 (클라이언트가 접속하는 번호와 같아야함)
+
+            // 문지기가 들고 있는 핸드폰
+            Socket listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            // 혹시 모르니 try catch
+            try
+            {
+                // 문지기 교육
+                listenSocket.Bind(endPoint);
+
+                // 영업 시작
+                // backlog = 최대 대기수(매개변수) 초과 시 fail 뜸
+                listenSocket.Listen(10);
+
+                // 영업을 손님 받을 때 까지 해야하니 무한루프
+                while (true)
+                {
+                    Console.WriteLine("Listening .... ");
+
+                    // 손님의 문의가 왔을 때 입장
+                    // -> 만약 손님이 입장을 안한다면? 실제는 이렇게 하진 않음 클라이언트가 입장안하면 아래는 안하고, 입장 해야만 할거임 
+                    Socket clientSocket = listenSocket.Accept(); // 리턴 값 소켓(손님의 친구)
+
+                    // 받는다
+                    byte[] recvBuff = new byte[1024]; // 좀 크게 생성
+                    int recvBytes = clientSocket.Receive(recvBuff); // 받은 정보는 recv에 넣고 몇 바이트인지는 recvBytes 저장
+                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);  // 변환하고자 하는 값, 시작 인덱스, 문자열 몇개짜리인지 설정
+                                                                                        // 문자열을 받는다는 가정이기에 이렇게 간단
+                                                                                        // 문자열로 소통하기에 endcording; UTF-8로 통일
+                    Console.WriteLine($"[From Client] {recvData}");
+                    // 보낸다
+
+                    byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to Justin Server"); // 문자열을 바로 보낼 수 있는 타입으로 바꿔준거
+                    clientSocket.Send(sendBuff);
+
+                    // 끝낸다.
+                    clientSocket.Shutdown(SocketShutdown.Both); // 조금 더 우아하게 쫓아내는 방법? 나중에 알려줌
+                    clientSocket.Close();
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
-
-
-//bool repeat = _threadLocal.IsValueCreated;
-
-//if (repeat)
-//    Console.WriteLine("중복임다" + _threadLocal.Value);
-//else
-//    Console.WriteLine(_threadLocal.Value);
