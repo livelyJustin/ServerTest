c39d657db29323f46c58322c40ece66411c808f6
liveljuseok <livelyjuseok@gmail.com>
2022-06-14 01:40:34 +0900

    Send Asyn 처리

--- ServerCore/Session.cs
+++ ServerCore/Session.cs
@@ -9,18 +9,24 @@ namespace ServerCore
         Socket _socket;
         int _disconnected = 0;
 
+        object _lock = new object();
+
+        // 실제로 바로 sendAsyn를 하는게 아나리 담아두었다가 꺼냈다가 할 큐
+        Queue<byte[]> _sendQueue = new Queue<byte[]>();
+        bool _pending = false; // 실행 중일 때는 바로 실행하지 않고, 큐에다가 저장해놓기 위함
+
+        // 재사용을 할 수 없다는게 단점 -> 전역 변수로 선언하여 사용
+        SocketAsyncEventArgs _sendArgs = new SocketAsyncEventArgs();
+
         public void Start(Socket socket)
         {
             _socket = socket;
             SocketAsyncEventArgs recvArgs = new SocketAsyncEventArgs();
+            
             // 자동 콜백
             recvArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnRecvCompleted);
+            _sendArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnSendCompleted);
 
-            // 유저 정보를 넣을 수 있음(식별자)
-            //            recvArgs.UserToken = this;
-            // receive는 버퍼를 만들어줘야함
-            // 버퍼의 크기는? 시작은? 상황에 따라 다를 수 있음)
-            // 버퍼를 크게 만들어서 세션끼리 나누어 사용할 수 있어 위치 필요 
             recvArgs.SetBuffer(new byte[1024], 0, 1024);
 
             RegisterRecv(recvArgs);
@@ -29,7 +35,15 @@ namespace ServerCore
         // 샌드는 리시브와 다르게 조금 더 복잡할 수 있음
         public void Send(byte[] sendbuff)
         {
-            _socket.Send(sendbuff);
+            _sendQueue.Enqueue(sendbuff);
+            // 멀티 쓰레드환경이기에 동시에 상호배제, 임계구역을 만들기 위한 lock
+            lock (_lock)
+            {
+                if (_pending == false) 
+                {
+                    RegisterSend();
+                }
+            }
         }
 
         // 동시에 Disconnect를 하거나
@@ -45,6 +59,51 @@ namespace ServerCore
         }
 
         #region 네트워크 통신
+        void RegisterSend()
+        {
+            // SEND()에서 LOCK 처리를 해주어 어짜피 한 명만 들어오기에 별도 LOCK 처리는 필요 없다.
+            _pending = true;
+            byte[] buff = _sendQueue.Dequeue();
+
+            _sendArgs.SetBuffer(buff, 0, buff.Length);
+
+            // args가 true되어 콜백으로 OnSend를 부르기 전에 Send()에서 lock을 뚫고,
+            // EnQueue 해준 애는 어디선가 누군가가 처리는 해주어야한다.
+            bool pending = _socket.SendAsync(_sendArgs);
+            if (pending == false)
+            {
+                OnSendCompleted(null, _sendArgs);
+            }
+        }
+
+        // 이벤트를 통한 동작, pending == false일 때 동작 총 2개의 방법으로 동작하기 lock 처리해주어야함
+        void OnSendCompleted(object sender, SocketAsyncEventArgs args)
+        {
+            lock (_lock)
+            {
+                if (args.BytesTransferred > 0 && args.SocketError == SocketError.Success)
+                {
+                    try
+                    {
+                        // queue에 남아있는 항목이 있다면 register에서 다시 뽑아서 사용
+                        if (_sendQueue.Count > 0)
+                            RegisterSend();
+                        else
+                            _pending = false;
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"OnSendCompleted Fail {e}");
+                    }
+                }
+                else
+                    Disconnect();
+            }
+        }
+        // 여기까지 들어왔다면 성공적으로 처리 된 것이니
+        // 다시 들어올 수 있게 pending을 false로 해준다.
+
+
         void RegisterRecv(SocketAsyncEventArgs args)
         {
             // 비동기
@@ -78,7 +137,7 @@ namespace ServerCore
             else
             {
                 // todo
-
+                Disconnect();
             }
         }
         #endregion
