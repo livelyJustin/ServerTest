9b12b34f6fff3c9ff3ba078e4d9054ed4ff124b7
liveljuseok <livelyjuseok@gmail.com>
2022-06-26 02:08:27 +0900

    String type Encoding (1)

--- DummyClient/ServerSession.cs
+++ DummyClient/ServerSession.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ServerCore;
+﻿using ServerCore;
 using System.Net;
-using System.Net.Sockets;
+using System.Text;
 
 namespace DummyClient
 {
@@ -25,6 +20,7 @@ namespace DummyClient
     class PlayerInforReq : Packet
     {
         public long playerId; // 8바이트
+        public string name;
 
         public PlayerInforReq()
         {
@@ -33,23 +29,30 @@ namespace DummyClient
 
         // wrtie는 직접 컨트롤하고 있기에 문제가 없지만 
         // read는 문제가 생길 수 있음 -> 서버는 항상 클라이언트 쪽에서 잘못된 정보를 보낼 수 있다고 가정하고 해야함
-        public override void Read(ArraySegment<byte> s)
+        public override void Read(ArraySegment<byte> segment)
         {
             ushort count = 0;
             //ushort size = BitConverter.ToUInt16(s.Array, s.Offset);
-            count += 2;
+
+            ReadOnlySpan<byte> readSpan = new ReadOnlySpan<byte>(segment.Array, segment.Offset, segment.Count);
+            count += sizeof(ushort);
             // ID는 추출할 필요가 없음 이미 Switch 문에서 걸렸다면 해당 아이디 이기 때매
             //ushort packetId = BitConverter.ToUInt16(s.Array, s.Offset + count);//하드 코딩말고 나중에 자동화할 예정
-            count += 2;
-            
+            count += sizeof(ushort);
+
             // 클라에서 보낸 정보를 무조건 Read 하는 것이 아니라, 크기가 정확한지 다시 한 번 체크한다.
             // 코드의 안정성을 위해 패킷의 사이즈를 한 번 더 체크해야함 ReadOnlySpan을 받는 값으로 변경해주어 체크 
 
             // s.count는 Write에서 count로 보낸 값. 즉, 12이다. 그렇기에 위에서 size, packetId를 추출하며 사용한 바이트 값을 제외하고
             // 남은 값의 크기가 일치하는 지 확인하는 것이다. 범위를 초과할 경우 에러가 날 것이다. 
-            this.playerId =  BitConverter.ToInt64(new ReadOnlySpan<byte>(s.Array, s.Offset + count, s.Count - count));
+            this.playerId = BitConverter.ToInt64(readSpan.Slice(count, readSpan.Length - count));
+            count += sizeof(long);
 
-            count += 8;
+            // string
+            ushort nameLen = BitConverter.ToUInt16(readSpan.Slice(count, readSpan.Length - count));
+            count += sizeof(ushort);
+            this.name = Encoding.Unicode.GetString(readSpan.Slice(count, nameLen)); // byte[] -> string
+
         }
 
         public override ArraySegment<byte> Write()
@@ -59,13 +62,34 @@ namespace DummyClient
             bool success = true;
             ushort count = 0;
 
-            count += 2;
+            Span<byte> span = new Span<byte>(openSeg.Array, openSeg.Offset, openSeg.Count);
+
+
+
+            // 하드 코딩 식이 아닌 더 깔끔한 표현을 위해 ushort의 사이즈를 올려준다.
+            count += sizeof(ushort);
             // this로 변경
-            success &= BitConverter.TryWriteBytes(new Span<byte>(openSeg.Array, openSeg.Offset + count, openSeg.Count - count), this.packetId);
-            count += 2;
-            success &= BitConverter.TryWriteBytes(new Span<byte>(openSeg.Array, openSeg.Offset + count, openSeg.Count - count), this.playerId);
-            count += 8;
-            success &= BitConverter.TryWriteBytes(new Span<byte>(openSeg.Array, openSeg.Offset, openSeg.Count), (ushort)4);
+            //success &= BitConverter.TryWriteBytes(new Span<byte>(openSeg.Array, openSeg.Offset + count, openSeg.Count - count), this.packetId);
+            // slice() => 시작 offest과 길이를 넘겨주어 해당 부분을 잘라서 넘겨준다. -> 가독성 증가
+            // slice는 결과가 변경 되는게 아닌 그 일부를 복사해 넘겨준다고 보면된다.
+            success &= BitConverter.TryWriteBytes(span.Slice(count, span.Length - count), this.packetId);
+
+            count += sizeof(ushort);
+            success &= BitConverter.TryWriteBytes(span.Slice(count, span.Length - count), this.playerId);
+            count += sizeof(long);
+
+            // string을 보냐기 위한 두 단계
+
+            //this.name.Length; => 길이는 4이나 8바이트가 나옴 
+            // utf-16으로 넘겨주는법 이렇게해야 4바이트가 넘어감
+            ushort nameLen = (ushort)Encoding.Unicode.GetByteCount(this.name);
+            success &= BitConverter.TryWriteBytes(span.Slice(count, span.Length - count), nameLen);
+            count += sizeof(ushort);
+            //Encoding.Unicode.GetBytes(this.name);   // string을 바이트 배열로 넘겨줄 수 있다.
+            Array.Copy(Encoding.Unicode.GetBytes(this.name), 0, openSeg.Array, count, nameLen);
+            count += nameLen;
+
+            success &= BitConverter.TryWriteBytes(span, count); // 원본을 넣으면 된다.
 
             // 성공 여부가 null 인지 아닌지로 알 수 있음
             if (success == false)
@@ -100,7 +124,7 @@ namespace DummyClient
             // 2바이트 + 2바이트 이니 4
 
             // 패킷id는 다른 곳에서 넣어줌
-            PlayerInforReq packet = new PlayerInforReq() {  playerId = 1001 };
+            PlayerInforReq packet = new PlayerInforReq() { playerId = 1001, name = "abcd" };
 
             {
                 // write() 함수에서 버퍼 크기 할당, 작업까지 다 해줌 
--- Server/ClientSessioncs.cs
+++ Server/ClientSessioncs.cs
@@ -1,5 +1,6 @@
 ﻿using ServerCore;
 using System.Net;
+using System.Text;
 
 namespace Server
 {
@@ -18,23 +19,39 @@ namespace Server
     class PlayerInforReq : Packet
     {
         public long playerId; // 8바이트
+        public string name;
 
         public PlayerInforReq()
         {
             this.packetId = (ushort)PacketID.PlayerInforReq;
         }
 
-        public override void Read(ArraySegment<byte> s)
+        // wrtie는 직접 컨트롤하고 있기에 문제가 없지만 
+        // read는 문제가 생길 수 있음 -> 서버는 항상 클라이언트 쪽에서 잘못된 정보를 보낼 수 있다고 가정하고 해야함
+        public override void Read(ArraySegment<byte> segment)
         {
             ushort count = 0;
             //ushort size = BitConverter.ToUInt16(s.Array, s.Offset);
-            count += 2;
+
+            ReadOnlySpan<byte> readSpan = new ReadOnlySpan<byte>(segment.Array, segment.Offset, segment.Count);
+            count += sizeof(ushort);
             // ID는 추출할 필요가 없음 이미 Switch 문에서 걸렸다면 해당 아이디 이기 때매
             //ushort packetId = BitConverter.ToUInt16(s.Array, s.Offset + count);//하드 코딩말고 나중에 자동화할 예정
-            count += 2;
-            this.playerId = BitConverter.ToInt64(new ReadOnlySpan<byte>(s.Array, s.Offset + count, s.Count - count));
+            count += sizeof(ushort);
 
-            count += 8;
+            // 클라에서 보낸 정보를 무조건 Read 하는 것이 아니라, 크기가 정확한지 다시 한 번 체크한다.
+            // 코드의 안정성을 위해 패킷의 사이즈를 한 번 더 체크해야함 ReadOnlySpan을 받는 값으로 변경해주어 체크 
+
+            // s.count는 Write에서 count로 보낸 값. 즉, 12이다. 그렇기에 위에서 size, packetId를 추출하며 사용한 바이트 값을 제외하고
+            // 남은 값의 크기가 일치하는 지 확인하는 것이다. 범위를 초과할 경우 에러가 날 것이다. 
+            this.playerId = BitConverter.ToInt64(readSpan.Slice(count, readSpan.Length - count));
+            count += sizeof(long);
+
+            // string
+            ushort nameLen = BitConverter.ToUInt16(readSpan.Slice(count, readSpan.Length - count));
+            count += sizeof(ushort);
+            this.name = Encoding.Unicode.GetString(readSpan.Slice(count, nameLen)); // byte[] -> string
+
         }
 
         public override ArraySegment<byte> Write()
@@ -44,13 +61,34 @@ namespace Server
             bool success = true;
             ushort count = 0;
 
-            count += 2;
+            Span<byte> span = new Span<byte>(openSeg.Array, openSeg.Offset, openSeg.Count);
+
+
+
+            // 하드 코딩 식이 아닌 더 깔끔한 표현을 위해 ushort의 사이즈를 올려준다.
+            count += sizeof(ushort);
             // this로 변경
-            success &= BitConverter.TryWriteBytes(new Span<byte>(openSeg.Array, openSeg.Offset + count, openSeg.Count - count), this.packetId);
-            count += 2;
-            success &= BitConverter.TryWriteBytes(new Span<byte>(openSeg.Array, openSeg.Offset + count, openSeg.Count - count), this.playerId);
-            count += 8;
-            success &= BitConverter.TryWriteBytes(new Span<byte>(openSeg.Array, openSeg.Offset, openSeg.Count), count);
+            //success &= BitConverter.TryWriteBytes(new Span<byte>(openSeg.Array, openSeg.Offset + count, openSeg.Count - count), this.packetId);
+            // slice() => 시작 offest과 길이를 넘겨주어 해당 부분을 잘라서 넘겨준다. -> 가독성 증가
+            // slice는 결과가 변경 되는게 아닌 그 일부를 복사해 넘겨준다고 보면된다.
+            success &= BitConverter.TryWriteBytes(span.Slice(count, span.Length - count), this.packetId);
+
+            count += sizeof(ushort);
+            success &= BitConverter.TryWriteBytes(span.Slice(count, span.Length - count), this.playerId);
+            count += sizeof(long);
+
+            // string을 보냐기 위한 두 단계
+
+            //this.name.Length; => 길이는 4이나 8바이트가 나옴 
+            // utf-16으로 넘겨주는법 이렇게해야 4바이트가 넘어감
+            ushort nameLen = (ushort)Encoding.Unicode.GetByteCount(this.name);
+            success &= BitConverter.TryWriteBytes(span.Slice(count, span.Length - count), nameLen);
+            count += sizeof(ushort);
+            //Encoding.Unicode.GetBytes(this.name);   // string을 바이트 배열로 넘겨줄 수 있다.
+            Array.Copy(Encoding.Unicode.GetBytes(this.name), 0, openSeg.Array, count, nameLen);
+            count += nameLen;
+
+            success &= BitConverter.TryWriteBytes(span, count); // 원본을 넣으면 된다.
 
             // 성공 여부가 null 인지 아닌지로 알 수 있음
             if (success == false)
@@ -59,7 +97,6 @@ namespace Server
             // 리턴 값 설정
             return SendBufferHelper.Close(count);
         }
-
     }
     public enum PacketID
     {
@@ -107,7 +144,7 @@ namespace Server
                     {
                         PlayerInforReq playerinfoReq = new PlayerInforReq();
                         playerinfoReq.Read(buffer);
-                        Console.WriteLine($"PlayerInforReq: {playerinfoReq.playerId}");
+                        Console.WriteLine($"PlayerInforReq: {playerinfoReq.playerId} playernanme: {playerinfoReq.name}");
                     }
                     break;
             }
