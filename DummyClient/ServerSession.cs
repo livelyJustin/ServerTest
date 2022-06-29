522deb19199042c112f8be332807d357d3e62776
liveljuseok <livelyjuseok@gmail.com>
2022-06-29 16:31:49 +0900

    Packet Session list added

--- DummyClient/ServerSession.cs
+++ DummyClient/ServerSession.cs
@@ -7,25 +7,60 @@ namespace DummyClient
     // 패킷 헤더
     public abstract class Packet
     {
-        // 크게 사용성이 없기에 제거해도 괜찮다. size, packetId 
-        public ushort size;
+        public ushort siez;
         public ushort packetId;
 
-        // send 생성을 하기 위한 클래스 생성
         public abstract ArraySegment<byte> Write();
         public abstract void Read(ArraySegment<byte> s);
     }
 
-    // 실제 처럼 하기 위한 class
+
     class PlayerInforReq : Packet
     {
         public long playerId; // 8바이트
         public string name; // string 뿐만 아니라 아이콘과 같은 byte 배열도 넘기는 방법을 이해한 것이다.(두 단계로 보낸방법)
 
+        public List<SkillInfo> skills = new List<SkillInfo>();
+
         public PlayerInforReq()
         {
             this.packetId = (ushort)PacketID.PlayerInforReq;
         }
+
+        public struct SkillInfo
+        {
+            public int id;
+            public short level;
+            public float duration;
+
+            public bool Write(Span<byte> span, ref ushort count)
+            {
+                bool success = true;
+
+                success &= BitConverter.TryWriteBytes(span.Slice(count, span.Length - count), id);
+                count += sizeof(int);
+
+                success &= BitConverter.TryWriteBytes(span.Slice(count, span.Length - count), level);
+                count += sizeof(short);
+
+                success &= BitConverter.TryWriteBytes(span.Slice(count, span.Length - count), duration);
+                count += sizeof(float);
+                return success;
+            }
+
+            public void Read(ReadOnlySpan<byte> readSpan, ref ushort count)
+            {
+                id = BitConverter.ToInt32(readSpan.Slice(count, readSpan.Length - count));
+                count += sizeof(int);
+
+                level = BitConverter.ToInt16(readSpan.Slice(count, readSpan.Length - count));
+                count += sizeof(short);
+
+                duration = BitConverter.ToSingle(readSpan.Slice(count, readSpan.Length - count));
+                count += sizeof(float);
+            }
+        }
+
 
         // wrtie는 직접 컨트롤하고 있기에 문제가 없지만 
         // read는 문제가 생길 수 있음 -> 서버는 항상 클라이언트 쪽에서 잘못된 정보를 보낼 수 있다고 가정하고 해야함
@@ -37,14 +72,27 @@ namespace DummyClient
 
             count += sizeof(ushort);
             count += sizeof(ushort);
-
             this.playerId = BitConverter.ToInt64(readSpan.Slice(count, readSpan.Length - count));
             count += sizeof(long);
 
             // string
             ushort nameLen = BitConverter.ToUInt16(readSpan.Slice(count, readSpan.Length - count));
             count += sizeof(ushort);
+
             this.name = Encoding.Unicode.GetString(readSpan.Slice(count, nameLen)); // byte[] -> string
+            count += nameLen;
+
+            // skill
+            skills.Clear();
+            ushort skillLeng = BitConverter.ToUInt16(readSpan.Slice(count, readSpan.Length - count));
+            count += sizeof(ushort);
+
+            for (int i = 0; i < skillLeng; i++)
+            {
+                SkillInfo skill = new SkillInfo();
+                skill.Read(readSpan, ref count);
+                skills.Add(skill);
+            }
 
         }
 
@@ -55,51 +103,34 @@ namespace DummyClient
             bool success = true;
             ushort count = 0;
 
-
-
-
-            // 하드 코딩 식이 아닌 더 깔끔한 표현을 위해 ushort의 사이즈를 올려준다.
-            // this로 변경
-            
-            //success &= BitConverter.TryWriteBytes(new Span<byte>(openSeg.Array, openSeg.Offset + count, openSeg.Count - count), this.packetId);
-
-
-            // slice() => 시작 offest과 길이를 넘겨주어 해당 부분을 잘라서 넘겨준다. -> 가독성 증가
-            // slice는 원본이 변경 되는게 아닌 그 일부를 복사해 넘겨준다.
-            
             Span<byte> span = new Span<byte>(openSeg.Array, openSeg.Offset, openSeg.Count);
+
 
             count += sizeof(ushort);
             success &= BitConverter.TryWriteBytes(span.Slice(count, span.Length - count), this.packetId);
 
             count += sizeof(ushort);
             success &= BitConverter.TryWriteBytes(span.Slice(count, span.Length - count), this.playerId);
-            
+
             count += sizeof(long);
 
-            // string을 보냐기 위한 두 단계
-
-            //this.name.Length; => 길이는 4이나 8바이트가 나옴 
-            // utf-16으로 넘겨주는법 이렇게해야 4바이트가 넘어감
-
-            //ushort nameLen = (ushort)Encoding.Unicode.GetByteCount(this.name);
-            //success &= BitConverter.TryWriteBytes(span.Slice(count, span.Length - count), nameLen);
-
-            //count += sizeof(ushort);
-            //Array.Copy(Encoding.Unicode.GetBytes(this.name), 0, openSeg.Array, count, nameLen);
-
-            // Array.Copy 시에는 getbytes로 내부적으로는 new를 하고 뱉어주고 있기에 상태이기에 위와 통일감 또는 효율적이게 작용하기 위해 사용함
-
-            // 이렇게하면 openSeg 배열의 offset+count 위치에 this.name 인코딩 값이 들어감
-            // 그거에 대한 총 길이가 ushort 타입으로 캐스팅하여 담아둠
-
+            // string
             ushort nameLen = (ushort)Encoding.Unicode.GetBytes(this.name, 0, this.name.Length, openSeg.Array, openSeg.Offset + count + sizeof(ushort));
-            
             success &= BitConverter.TryWriteBytes(span.Slice(count, span.Length - count), nameLen);
-
-            count += nameLen;
             count += sizeof(ushort);
+            count += nameLen;
+
+            // skill
+            // skill의 count를 ushort 타입으로 넣어줘 list가 얼마나 들어갈지 말해주기
+            success &= BitConverter.TryWriteBytes(span.Slice(count, span.Length - count), (ushort)skills.Count);
+            count += sizeof(ushort);
+
+            // 하나씩 돌며 데이터 넣기
+            foreach (SkillInfo skill in skills)
+                success &= skill.Write(span, ref count);
+
             success &= BitConverter.TryWriteBytes(span, count); // 원본을 넣으면 된다.
+
 
 
             // 성공 여부가 null 인지 아닌지로 알 수 있음
@@ -120,22 +151,17 @@ namespace DummyClient
 
     class ServerSession : Session
     {
-        // writebytes가 유니티에서 안될경우 이렇게도 됨
-        // unsafe는 포인터 조작가능 -> c++ 과 같은 방법으로 만들어서 사용도 됨. 속도 증가
-        // array[offset] = value 개념
-        //static unsafe void ToBytes(byte[] array, int offest, ulong value)
-        //{
-        //    fixed (byte* ptr = &array[offest])
-        //        *(ulong*)ptr = value;
-        //}
 
         public override void OnConnected(EndPoint end)
         {
             Console.WriteLine($"OnConnected : {end}");
             // 2바이트 + 2바이트 이니 4
 
-            // 패킷id는 다른 곳에서 넣어줌
             PlayerInforReq packet = new PlayerInforReq() { playerId = 1001, name = "저스틴" };
+            packet.skills.Add(new PlayerInforReq.SkillInfo() { id = 111, level = 10, duration = 3f });
+            packet.skills.Add(new PlayerInforReq.SkillInfo() { id = 222, level = 20, duration = 4f });
+            packet.skills.Add(new PlayerInforReq.SkillInfo() { id = 333, level = 30, duration = 5f });
+            packet.skills.Add(new PlayerInforReq.SkillInfo() { id = 444, level = 40, duration = 6f });
 
             {
                 // write() 함수에서 버퍼 크기 할당, 작업까지 다 해줌 
--- Server/ClientSessioncs.cs
+++ Server/ClientSessioncs.cs
@@ -7,24 +7,60 @@ namespace Server
     // 패킷 헤더
     public abstract class Packet
     {
-        public ushort size;
+        public ushort siez;
         public ushort packetId;
 
-        // send 생성을 하기 위한 클래스 생성
         public abstract ArraySegment<byte> Write();
         public abstract void Read(ArraySegment<byte> s);
     }
 
-    // 실제 처럼 하기 위한 class
+
     class PlayerInforReq : Packet
     {
         public long playerId; // 8바이트
         public string name; // string 뿐만 아니라 아이콘과 같은 byte 배열도 넘기는 방법을 이해한 것이다.(두 단계로 보낸방법)
 
+        public List<SkillInfo> skills = new List<SkillInfo>();
+
         public PlayerInforReq()
         {
             this.packetId = (ushort)PacketID.PlayerInforReq;
         }
+
+        public struct SkillInfo 
+        {
+            public int id;
+            public short level;
+            public float duration;
+
+            public bool Write(Span<byte> span, ref ushort count)
+            {
+                bool success = true;
+
+                success &= BitConverter.TryWriteBytes(span.Slice(count, span.Length - count), id);
+                count += sizeof(int);
+
+                success &= BitConverter.TryWriteBytes(span.Slice(count, span.Length - count), level);
+                count += sizeof(short);
+
+                success &= BitConverter.TryWriteBytes(span.Slice(count, span.Length - count), duration);
+                count += sizeof(float);
+                return success;
+            }
+
+            public void Read(ReadOnlySpan<byte> readSpan, ref ushort count)
+            {
+                id = BitConverter.ToInt32(readSpan.Slice(count, readSpan.Length - count));
+                count += sizeof(int);
+
+                level = BitConverter.ToInt16(readSpan.Slice(count, readSpan.Length - count));
+                count += sizeof(short);
+
+                duration = BitConverter.ToSingle(readSpan.Slice(count, readSpan.Length - count));
+                count += sizeof(float);
+            }
+        }
+
 
         // wrtie는 직접 컨트롤하고 있기에 문제가 없지만 
         // read는 문제가 생길 수 있음 -> 서버는 항상 클라이언트 쪽에서 잘못된 정보를 보낼 수 있다고 가정하고 해야함
@@ -36,14 +72,27 @@ namespace Server
 
             count += sizeof(ushort);
             count += sizeof(ushort);
-
             this.playerId = BitConverter.ToInt64(readSpan.Slice(count, readSpan.Length - count));
             count += sizeof(long);
 
             // string
             ushort nameLen = BitConverter.ToUInt16(readSpan.Slice(count, readSpan.Length - count));
             count += sizeof(ushort);
+
             this.name = Encoding.Unicode.GetString(readSpan.Slice(count, nameLen)); // byte[] -> string
+            count += nameLen;
+
+            // skill
+            skills.Clear();
+            ushort skillLeng = BitConverter.ToUInt16(readSpan.Slice(count, readSpan.Length - count));
+            count += sizeof(ushort);
+
+            for (int i = 0; i < skillLeng; i++)
+            {
+                SkillInfo skill = new SkillInfo();
+                skill.Read(readSpan, ref count);
+                skills.Add(skill);
+            }
 
         }
 
@@ -53,18 +102,6 @@ namespace Server
 
             bool success = true;
             ushort count = 0;
-
-
-
-
-            // 하드 코딩 식이 아닌 더 깔끔한 표현을 위해 ushort의 사이즈를 올려준다.
-            // this로 변경
-
-            //success &= BitConverter.TryWriteBytes(new Span<byte>(openSeg.Array, openSeg.Offset + count, openSeg.Count - count), this.packetId);
-
-
-            // slice() => 시작 offest과 길이를 넘겨주어 해당 부분을 잘라서 넘겨준다. -> 가독성 증가
-            // slice는 원본이 변경 되는게 아닌 그 일부를 복사해 넘겨준다.
 
             Span<byte> span = new Span<byte>(openSeg.Array, openSeg.Offset, openSeg.Count);
 
@@ -76,29 +113,25 @@ namespace Server
 
             count += sizeof(long);
 
-            // string을 보냐기 위한 두 단계
-
-            //this.name.Length; => 길이는 4이나 8바이트가 나옴 
-            // utf-16으로 넘겨주는법 이렇게해야 4바이트가 넘어감
-
-            //ushort nameLen = (ushort)Encoding.Unicode.GetByteCount(this.name);
-            //success &= BitConverter.TryWriteBytes(span.Slice(count, span.Length - count), nameLen);
-
-            //count += sizeof(ushort);
-            //Array.Copy(Encoding.Unicode.GetBytes(this.name), 0, openSeg.Array, count, nameLen);
-
-            // Array.Copy 시에는 getbytes로 내부적으로는 new를 하고 뱉어주고 있기에 상태이기에 위와 통일감 또는 효율적이게 작용하기 위해 사용함
-
-            // 이렇게하면 openSeg 배열의 offset+count 위치에 this.name 인코딩 값이 들어감
-            // 그거에 대한 총 길이가 ushort 타입으로 캐스팅하여 담아둠
-
+            // string
             ushort nameLen = (ushort)Encoding.Unicode.GetBytes(this.name, 0, this.name.Length, openSeg.Array, openSeg.Offset + count + sizeof(ushort));
-
             success &= BitConverter.TryWriteBytes(span.Slice(count, span.Length - count), nameLen);
-
-            count += nameLen;
             count += sizeof(ushort);
+            count += nameLen;
+
+            // skill
+
+            // skill의 count를 ushort 타입으로 넣어줘 list가 얼마나 들어갈지 말해주기
+            success &= BitConverter.TryWriteBytes(span.Slice(count, span.Length - count), (ushort)skills.Count);
+            count += sizeof(ushort);
+
+
+            // 하나씩 돌며 데이터 넣기
+            foreach (SkillInfo skill in skills)
+                success &= skill.Write(span, ref count);
+
             success &= BitConverter.TryWriteBytes(span, count); // 원본을 넣으면 된다.
+
 
 
             // 성공 여부가 null 인지 아닌지로 알 수 있음
@@ -109,6 +142,7 @@ namespace Server
             return SendBufferHelper.Close(count);
         }
     }
+
 
     public enum PacketID
     {
@@ -144,9 +178,9 @@ namespace Server
         {
             ushort count = 0;
             ushort size = BitConverter.ToUInt16(buffer.Array, buffer.Offset);
-            count += 2;
+            count += (ushort)sizeof(ushort);
             ushort packetId = BitConverter.ToUInt16(buffer.Array, buffer.Offset + count);//하드 코딩말고 나중에 자동화할 예정
-            count += 2;
+            count += (ushort)sizeof(ushort);
 
             // packetId를 PacketID Class 타입으로 캐스트 한 걸 검사
             // 자동화 고려
@@ -157,6 +191,12 @@ namespace Server
                         PlayerInforReq playerinfoReq = new PlayerInforReq();
                         playerinfoReq.Read(buffer);
                         Console.WriteLine($"PlayerInforReq: {playerinfoReq.playerId} playernanme: {playerinfoReq.name}");
+
+                        foreach (PlayerInforReq.SkillInfo skill in playerinfoReq.skills)
+                        {
+                            Console.WriteLine($"skill_Id {skill.id}, skill_level {skill.level}, skill_duration {skill.duration} ");
+                        }
+
                     }
                     break;
             }
